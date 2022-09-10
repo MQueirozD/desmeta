877281bf9f09b2fd708697ea03661521927fb06c
Matues <mateus.q.d@gmail.com>
2022-09-10 13:54:06 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationButton from "./componetes/NotificationButton"
+
 function App() {
  return(
+  <>
   <h1>Ola mundo</h1>
-  
+  <NotificationButton/>
+  <NotificationButton/>
+  <NotificationButton/>
+  <NotificationButton/>
+  </>
  )
 }
 
