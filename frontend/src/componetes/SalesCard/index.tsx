9fae6871f4f7db9f9d4f30d549a3c2e8cce1e880
Matues <mateus.q.d@gmail.com>
2022-09-10 21:00:35 -0300

    Sale listing

--- frontend/src/componetes/SalesCard/index.tsx
+++ frontend/src/componetes/SalesCard/index.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
 
 import NotificationButton from '../NotificationButton';
 import './styles.css';
@@ -12,14 +14,17 @@ function SalesCard() {
     //criamos um calendario de 1ano antes!
 
 
-    const [minDate, setMinDate] = useState (min);//set igual do java(onde conseguimos alterar)
-    const [maxDate, setMaxDate] = useState (max);
+    const [minDate, setMinDate] = useState(min);//set igual do java(onde conseguimos alterar)
+    const [maxDate, setMaxDate] = useState(max);
 
-    useEffect (() => {
-        axios.get("http://localhost:8080/sales")
-        .then(response => {
-            console.log(response.data)
-        })
+    const [sales, setSales] = useState<Sale[]>([]);//<Sale[]> - é uma lista(uma lista de Sale)
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales`)//chama aqui o resquest.ts
+            .then(response => {//resposta com sucesso
+                setSales(response.data.content);
+                
+            })
     }, [])
 
 
@@ -31,7 +36,7 @@ function SalesCard() {
                     <div className="dsmeta-form-control-container">
                         <DatePicker
                             selected={minDate}
-                            onChange={(date: Date) => setMinDate (date)} //seleciona e muda a data
+                            onChange={(date: Date) => setMinDate(date)} //seleciona e muda a data
                             className="dsmeta-form-control"
                             dateFormat="dd/MM/yyyy"
                         />
@@ -39,7 +44,7 @@ function SalesCard() {
                     <div className="dsmeta-form-control-container">
                         <DatePicker
                             selected={maxDate}
-                            onChange={(date: Date) => setMaxDate (date)}
+                            onChange={(date: Date) => setMaxDate(date)}
                             className="dsmeta-form-control"
                             dateFormat="dd/MM/yyyy"
                         />
@@ -58,47 +63,27 @@ function SalesCard() {
                                 <th>Total</th>
                                 <th>Notificar</th>
                             </tr>
+
                         </thead>
                         <tbody>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
+                            {sales.map(sale => {
+                                return (
+                                    <tr key={sale.id}>
+                                        <td className="show992">{sale.id}</td>
+                                        <td className="show576">{new Date(sale.date)
+                                            .toLocaleDateString()}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>R$ {sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <div className="dsmeta-red-btn-container">
+                                                <NotificationButton />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
                         </tbody>
 
                     </table>
---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+//pega o valor url caso não va para a url "http://localhost:8080"
